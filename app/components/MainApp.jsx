1077eab11b1504c58ffc065ffb233507a78b86c2
toxcik3110 <toxcik3110@bigmir.net>
2017-08-06 15:06:18 +0300

    Fixed algorithm

--- app/components/MainApp.jsx
+++ app/components/MainApp.jsx
@@ -20,16 +20,48 @@ class MainApp extends React.Component {
 		// 	}
 		// 	arr.push(buf);
 		// }
+		// for (var i = 0; i < 9; i++) {
+		// 	arr[i][i].num = 1;
+		// }
+		// var buf = [[4,0,1,0,0,0,0,3,5],
+		// 		   [3,9,0,0,0,4,8,2,0],
+		// 		   [0,7,0,0,8,6,0,0,9],
+		// 		   [0,4,5,7,0,8,0,0,0],
+		// 		   [0,0,9,0,6,0,5,0,0],
+		// 		   [0,0,0,5,0,9,7,6,0],
+		// 		   [8,0,0,9,2,0,0,1,0],
+		// 		   [0,2,3,6,0,0,0,7,8],
+		// 		   [9,6,0,0,0,0,2,0,3]];
 
-		var buf = [[4,0,1,0,0,0,0,3,5],
-				   [3,9,0,0,0,4,8,2,0],
-				   [0,7,0,0,8,6,0,0,9],
-				   [0,4,5,7,0,8,0,0,0],
-				   [0,0,9,0,6,0,5,0,0],
-				   [0,0,0,5,0,9,7,6,0],
-				   [8,0,0,9,2,0,0,1,0],
-				   [0,2,3,6,0,0,0,7,8],
-				   [9,6,0,0,0,0,2,0,3]];
+		// var buf = [[5,3,0,2,6,0,1,0,7],
+		// 		   [6,1,0,0,0,9,0,0,4],
+		// 		   [0,0,9,0,0,8,0,3,0],
+		// 		   [2,5,0,0,9,0,6,4,0],
+		// 		   [0,0,0,0,0,0,0,0,0],
+		// 		   [0,9,6,0,2,0,0,8,1],
+		// 		   [0,4,0,1,0,0,5,0,0],
+		// 		   [8,0,0,3,0,0,0,7,9],
+		// 		   [7,0,5,0,4,2,0,1,8]];
+
+		// var buf = [[5,4,0,1,2,0,8,0,6],
+		// 		   [7,2,0,0,0,6,0,0,5],
+		// 		   [0,0,8,0,0,4,0,2,0],
+		// 		   [1,6,0,0,4,0,2,3,0],
+		// 		   [0,0,0,0,0,0,0,0,0],
+		// 		   [0,3,9,0,5,0,0,6,8],
+		// 		   [0,1,0,4,0,0,6,0,0],
+		// 		   [9,0,0,2,0,0,0,5,3],
+		// 		   [2,0,6,0,8,7,0,1,9]];
+
+		var buf = [[0,0,9,8,0,6,1,0,0],
+				   [0,7,0,0,0,9,3,8,0],
+				   [6,1,0,0,3,0,0,0,4],
+				   [2,5,0,0,8,0,0,0,9],
+				   [0,0,6,1,0,4,5,0,0],
+				   [8,0,0,0,5,0,0,6,3],
+				   [1,0,0,0,6,0,0,4,7],
+				   [0,6,3,7,0,0,0,2,0],
+				   [0,0,4,5,0,2,6,0,0]];
 
 		// var buf = 
 		// 	  [[4,0,1,
@@ -77,7 +109,7 @@ class MainApp extends React.Component {
 		}
 	}
 	render() {
-		// console.log('bigGrid',this.state.grid);
+		console.log('bigGrid',this.state.grid);
 		var solveSudoku = () => {
 			var arr = [...this.state.grid];
 			console.log('arr', arr)
@@ -143,8 +175,8 @@ class MainApp extends React.Component {
 					if(arr[index][i].num === 0) {
 						var freeH = checkHorizontal(index);
 						var freeV = checkVertical(i);
-						if(i == 0)
-						console.log('checkVertical ' + index + ' ' + i,freeV);
+						// if(i == 0)
+						// console.log('checkVertical ' + index + ' ' + i,freeV);
 						var freeS = checkSquare(index,i);
 						var free = arrayIntersect(freeV, arrayIntersect(freeS, freeH));
 						// console.log('buf',`${free}`);
@@ -178,30 +210,102 @@ class MainApp extends React.Component {
 				}
 				// return result;
 				var res = [];
+				console.log('result',result)
 				for (var i = 0; i < result.length; i++) {
-					var available = ['1','2','3','4','5','6','7','8','9']
+					var available = ['1','2','3','4','5','6','7','8','9'];
 					var boo = true;
 					for (var j = 0; j < available.length; j++) {
 						var regExp = new RegExp(available[j] + '', 'g');
 						var find = result[i].match(regExp);
+						// console.log(find)
 						if(find !== null) {
 							if(find.length > 1) boo = false;
 						}
 					}
-					if(boo) res.push(result[i]);
+					if(boo) {
+						console.log(boo)
+						res.push(result[i]);
+					}
 				}
+				console.log(res)
 				return res[0];
 			}
 
-			for(var i = 0; i < 9; i++) {
-				var buf = checkEmpty(i);
-				// console.log('row',buf);
-				var res = composeVariants(buf);
-				for(var j = 0; j < buf.length; j++) {
-					arr[buf[j].y][buf[j].x].num = +res[j];
+			var iterThis = () => {
+				var answer = [];
+				for(var i = 0; i < 9; i++) {
+					var buf = checkEmpty(i);
+					// console.log('row',buf);
+					// var res = composeVariants(buf);
+					// for(var j = 0; j < buf.length; j++) {
+					// 	arr[buf[j].y][buf[j].x].num = +res[j];
+					// }
+					// console.log('res',res)
+					answer = [...answer, [...buf]];
+					// console.log('answerW', answer)
+					// console.log('res',res);
 				}
-				// console.log('res',res);
+				return answer;
 			}
+			var answer = [];
+			answer = iterThis();
+			// console.log('answer', answer)
+			for(var i = 0; i < answer.length; i++) {
+				for(var j = 0; j < answer[i].length; j++) {
+					var str = '';
+					// for (var k = 0; k < answer[i][j].available.length; k++) {
+						// str += `${answer[i][j].available[k]}, `;
+					// }
+					if(answer[i][j].available.length == 1) {
+						arr[answer[i][j].y][answer[i][j].x].num = answer[i][j].available[0];
+					}
+					// console.log(answer[i][j].x + ':' + answer[i][j].y + '=' + str)
+					// arr[answer[i][j].y][answer[i][j].x].num = str;
+				}
+			}
+
+			var boo = 0;
+
+			while(boo < 9) {
+				console.log('boo cycle')
+				boo = 0;
+				answer = iterThis();
+				// console.log('answer', answer)
+				for(var i = 0; i < answer.length; i++) {
+					if(answer[i].length == 0) {
+						boo++;
+					}
+					for(var j = 0; j < answer[i].length; j++) {
+						var str = '';
+						// for (var k = 0; k < answer[i][j].available.length; k++) {
+							// str += `${answer[i][j].available[k]}, `;
+						// }
+						if(answer[i][j].available.length == 1) {
+							arr[answer[i][j].y][answer[i][j].x].num = answer[i][j].available[0];
+						}
+						// console.log(answer[i][j].x + ':' + answer[i][j].y + '=' + str)
+						// arr[answer[i][j].y][answer[i][j].x].num = str;
+					}
+				}
+			}
+
+			console.log('answer', answer)
+			for(var i = 0; i < answer.length; i++) {
+				for(var j = 0; j < answer[i].length; j++) {
+					var str = '';
+					for (var k = 0; k < answer[i][j].available.length; k++) {
+						str += `${answer[i][j].available[k]}, `;
+					}
+					// if(answer[i][j].available.length == 1) {
+					// 	arr[answer[i][j].y][answer[i][j].x].num = answer[i][j].available[0];
+					// }
+					// console.log(answer[i][j].x + ':' + answer[i][j].y + '=' + str)
+					arr[answer[i][j].y][answer[i][j].x].num = str;
+				}
+			}
+
+			// console.log('arr', arr)
+			// console.log()
 
 			this.setState({
 				grid:[...arr],
